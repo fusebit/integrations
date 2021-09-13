8748b5218c1067a611ca9978bea3700f46d42835
degrammer <ruben@fusebit.io>
2021-09-13 17:00:59 -0500

    Skip Husky in publish step

--- scripts/prepare_publish.sh
+++ scripts/prepare_publish.sh
@@ -2,8 +2,9 @@
 set -e
 echo "Running prepare publish script"
 export HUSKY="0"
-echo "HUSKY hooks disabled"
+echo "Husky hooks disabled"
+echo "Registry to publish:$PUBLISH_REGISTRY"
 lerna run lint
 lerna run prettier:check
 lerna run build
-lerna publish --registry=$PUBLISH_REGISTRY
+lerna publish --registry=$PUBLISH_REGISTRY --force-publish
