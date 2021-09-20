497fc4f3a95a0eb8e23217057a17847003de830a
Matthew Zhao <matthew@fusebit.io>
2021-09-20 13:55:21 -0700

    huh

--- scripts/cicd/remote_ref_prettier.sh
+++ scripts/cicd/remote_ref_prettier.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+echo $GITHUB_BASE_REF
 git ls-tree -r $GITHUB_BASE_REF --name-only | grep -E \"\\.[tj]?sx?$|\\.yaml$|\\.json$\" | grep -v assets | grep -v lerna.json | xargs -P 1 prettier --check
