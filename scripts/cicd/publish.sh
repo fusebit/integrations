579dd8d81cfecd61c3f8ffda7daaf3a80d092817
Matthew Zhao <matthew@fusebit.io>
2021-09-21 10:36:50 -0700

    .

---
+++ scripts/cicd/publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Set standard bash debug envs
+set -x
+
+# install fusebit tools
+
+npm i -g @fusebit/cli
+
+# start publish
+
+for i in $FUSE_PROFILE_INTERNAL_LIST
+do
+    echo $i
+    echo $i | fuse profile import $i
+    fuse profile get -o json
+done
