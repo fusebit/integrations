5bee3b9f29168a26b436e6c1d27123e3342b7f08
Matthew Zhao <matthew@fusebit.io>
2021-09-21 10:20:37 -0700

    .

--- scripts/cicd/lint.sh
+++ scripts/cicd/lint.sh
@@ -4,3 +4,4 @@ set -xe
 npm ci
 npx lerna bootstrap
 git ls-tree -r HEAD --name-only | grep -E ".[tj]?sx?$|.yaml$|.json$" | grep -v assets | grep -v lerna.json | xargs -P 1 npx prettier --check
+npx lerna lint:check
