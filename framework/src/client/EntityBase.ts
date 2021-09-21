f2449e2c32153a6680c9a3691cb42dd0fb9561b6
degrammer <ruben@fusebit.io>
2021-09-21 11:44:09 -0500

    Improve framework jsdocs

--- framework/src/client/EntityBase.ts
+++ framework/src/client/EntityBase.ts
@@ -48,32 +48,95 @@ namespace EntityBase {
   export abstract class ServiceBase {}
 
   export abstract class StorageBase {
+    /** Save any data in JSON format up to ~400Kb in size.
+     * @example
+     * ```
+     * router.post('/api/tenant/:tenantId/colors', async (ctx) => {
+     *    // By convention we use / symbol to represent a bucket, but you can use any name you want.
+     *    const bucketName = '/my-bucket/';
+     *    const key = 'colors';
+     *    const data = ['green', 'blue'];
+     *    const result = await integration.storage.setData(ctx, `${bucketName}${key}`, data);
+     *    ctx.body = result;
+     * });
+     * ```
+     * @param ctx The context object provided by the route function
+     * @param {string} dataKey Represents a reference to your data that you will use in further
+     * operations like read, delete and update
+     * @param {string} data Any valid JSON
+     * @returns {Promise<any>}
+     */
     public setData: (ctx: RouterContext, dataKey: string, data: any) => Promise<any> = async (
       ctx: RouterContext,
       dataKey: string,
       data: any,
       version?: string
     ) => Storage.createStorage(ctx.state.params).put(data, dataKey, version);
+
+    /** Get saved data
+     * @param ctx The context object provided by the route function
+     * @param {string} dataKey The key name used for referencing the stored data
+     * @returns {Promise<any>}
+     */
     public getData: (ctx: RouterContext, dataKey: string) => Promise<any> = async (
       ctx: RouterContext,
       dataKey: string
     ) => Storage.createStorage(ctx.state.params).get(dataKey);
+
+    /** A listing operation query data stored in an artifact known as a Bucket (Buckets are
+     * collections of keys where you can store related data). Read more at
+     * https://developer.fusebit.io/docs/integration-programming-model#listing-data
+     * @example
+     * ```
+     * router.get('/api/tenant/:tenantId/my-bucket', async (ctx) => {
+     *        const bucketName = '/my-bucket/';
+     *        const result = await integration.storage.listData(ctx, bucketName);
+     *        ctx.body = result;
+     * });
+     * ```
+     * @param ctx The context object provided by the route function
+     * @param {string} dataKeyPrefix The bucket name
+     * @param {Storage.IListOption} options The bucket name
+     * @returns {Promise<Storage.IStorageVersionedResponseList>} A list of Storage items
+     */
     public listData: (
       ctx: RouterContext,
       dataKeyPrefix: string,
       options?: Storage.IListOption
     ) => Promise<any> = async (ctx: RouterContext, dataKeyPrefix: string, options?: Storage.IListOption) =>
       Storage.createStorage(ctx.state.params).list(dataKeyPrefix, options);
+
+    /** Delete data
+     * @param ctx The context object provided by the route function
+     * @param {string} dataKey Reference the key name used for storing the data
+     * @param {string=} version Delete a specific version of the stored data
+     * @returns {Promise<Storage.IStorageVersionedResponseDelete>}
+     */
     public deleteData: (ctx: RouterContext, dataKey: string, version?: string) => Promise<any> = async (
       ctx: RouterContext,
       dataKey: string,
       version?: string
     ) => Storage.createStorage(ctx.state.params).delete(dataKey, version);
+
+    /** Delete data stored in an artifact known as a Bucket
+     * (This function will remove a collection of keys stored under the specified Bucket).
+     * @param ctx The context object provided by the route function
+     * @param {string} dataKeyPrefix The bucket name
+     * @param {string=} version Delete a specific version of the Bucket
+     * @returns {Promise<Storage.IStorageVersionedResponseDelete>}
+     */
     public deletePrefixedData: (ctx: RouterContext, dataKeyPrefix: string, version?: string) => Promise<any> = (
       ctx: RouterContext,
       dataKeyPrefix: string,
       version?: string
     ) => Storage.createStorage(ctx.state.params).deletePrefixed(dataKeyPrefix, version);
+
+    /** Recursively delete all storage objects in the Fusebit subscription.
+     * @param ctx The context object provided by the route function
+     * @param {boolean} forceDelete You need to force a delete (set to true),
+     * otherwise it will throw an error
+     * @returns {Promise<Storage.IStorageVersionedResponseDelete>}
+     */
     public deleteAllData: (ctx: RouterContext, forceDelete: boolean) => Promise<any> = (ctx, forceDelete) =>
       Storage.createStorage(ctx.state.params).deleteAll(forceDelete);
   }
--- framework/src/client/Integration.ts
+++ framework/src/client/Integration.ts
@@ -10,14 +10,33 @@ class Middleware extends EntityBase.MiddlewareBase {
 }
 
 export class Service extends EntityBase.ServiceBase {
+  /** Get an instantiated SDK for the specified Connector and Instance
+   * @param ctx The context object provided by the route function
+   * @param {string} connectorName The name of the connector from the service to interact with
+   * @param {string} instanceId The identifier of the Instance to get the associated connector
+   * @returns {Promise<any>} Returns an authenticated SDK you would use to interact with the
+   * connector service on behalf of your user
+   */
   public getSdk = async (ctx: RouterContext, connectorName: string, instanceId: string) => {
     return ctx.state.manager.connectors.getByName(ctx, connectorName, instanceId);
   };
 
+  /** Get an instantiated SDK for each specified Connector and Instance
+   * @param ctx The context object provided by the route function
+   * @param {string[]} connectorNames An array of Connector names
+   * @param {string} instanceId The identifier of the Instance to get the associated connectors
+   * @returns {Promise<any>[]} Returns an array of official Connector SDK instances
+   * already authorized with the proper credentials
+   */
   public getSdks = (ctx: RouterContext, connectorNames: string[], instanceId: string) => {
     return connectorNames.map((connectorName) => this.getSdk(ctx, connectorName, instanceId));
   };
 
+  /** Get a configured integration with a set of identity's
+   * and other values that represent a single user of the integration.
+   * @param ctx The context object provided by the route function
+   * @param {string} instanceId
+   */
   public getInstance = async (ctx: RouterContext, instanceId: string): Promise<EntityBase.Types.IInstance> => {
     const response = await superagent
       .get(`${ctx.state.params.baseUrl}/instance/${instanceId}`)
@@ -34,6 +53,15 @@ class Tenant {
     this.service = service;
   }
 
+  /**
+   * Get an official connector SDK instance, already authorized with the tenant's credentials
+   * @param ctx The context object provided by the route function
+   * @param {string} connectorName The name of the connector from the service to interact with
+   * @param {string} tenantId Represents a single user of this integration,
+   * usually corresponding to a user or account in your own system
+   * @returns Promise<any> Returns an authenticated SDK you would use to interact with the
+   * connector service on behalf of your user
+   */
   public getSdkByTenant = async (ctx: RouterContext, connectorName: string, tenantId: string) => {
     const response = await superagent
       .get(`${ctx.state.params.baseUrl}/instance?tag=${TENANT_TAG_NAME}=${tenantId}`)
