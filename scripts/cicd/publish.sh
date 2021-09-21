7503018608e998a6355f411a5a036a99c8a37fd4
Matthew Zhao <matthew@fusebit.io>
2021-09-21 11:16:03 -0700

    .

--- scripts/cicd/publish.sh
+++ scripts/cicd/publish.sh
@@ -7,6 +7,19 @@ set -x
 
 npm i -g @fusebit/cli
 
+# check if publish is nessersary
+VER_WART=v
+VERSION=$(cat lerna.json | jq -r .version)
+git tag --points-at HEAD | grep ${VER_WART}-${VERSION} > /dev/null
+TAG_TEST=$?
+if [ ${TAG_TEST} -ne 0 ]; then
+  echo "Not publishing ${VERSION} - HEAD is not tagged ${VER_WART}-${VERSION}"
+  git tag --points-at HEAD
+  exit 0;
+else
+  echo "Publishing ${VERSION}"
+fi
+
 # start publish
 
 for i in $FUSE_PROFILE_INTERNAL_LIST
@@ -14,4 +27,8 @@ do
     echo $i
     eval echo \$$i | fuse profile import $i
     fuse profile get -o json $i
+    fuse npm login -p $i
+    FUSE_PROFILE=$(fuse profile get -o json)
+    URL=$(echo $FUSE_PROFILE | jq -r .baseUrl)/v1/account/$(echo $FUSE_PROFILE | jq -r .account)/registry/default/npm
+    lerna publish from-git $URL
 done
