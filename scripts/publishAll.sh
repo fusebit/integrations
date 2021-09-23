7d40a9201f784d7a0040101269ffc9754cf1d6d6
Benn Bollay <bb.github@magitech.org>
2021-09-23 16:50:46 -0700

    .

--- scripts/publishAll.sh
+++ scripts/publishAll.sh
@@ -15,4 +15,4 @@ fuse npm login
 
 FUSE_PROFILE=$(fuse profile get -o json ${i})
 URL=$(echo $FUSE_PROFILE | jq -r .baseUrl)/v1/account/$(echo $FUSE_PROFILE | jq -r .account)/registry/default/npm
-lerna publish from-package --registry $URL --yes --ignore-scripts
+lerna publish from-package --registry $URL
