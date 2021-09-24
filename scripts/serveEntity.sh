ad7359897949952f6f3e2cb4be95db3c1f81bbd5
Benn Bollay <bb.github@magitech.org>
2021-09-24 10:26:16 -0700

    Resolve the package-not-found problem

--- scripts/serveEntity.sh
+++ scripts/serveEntity.sh
@@ -23,9 +23,21 @@ fi
 
 TARGETDIR=${PREFIX}/${ENTITYTYPE}-${ENTITYID}
 
+# Get the function into the TARGETDIR
 mkdir -p ${TARGETDIR}
-
 rm -rf ${TARGETDIR}
 fuse function get -b ${ENTITYTYPE} ${ENTITYID} -d ${TARGETDIR}
+
+# Add a 'name' to the package.json to avoid accidental 'unnamed' project confusion by lerna
+NEWPACKAGE=`cat ${TARGETDIR}/package.json | jq ".name = \"${ENTITYID}\""`
+echo ${NEWPACKAGE} > ${TARGETDIR}/package.json
+
+# Use lerna to create the symlinks for the function's node_modules directory
 lerna bootstrap
+
+# Include the node_modules for the entity in the search path, to aid resolution in framework of various
+# connectors and providers that are side-loaded via string-based require() statements.
+export NODE_PATH=${NODE_PATH}:${TARGETDIR}/node_modules
+
+# Serve the function
 fuse function serve -b ${ENTITYTYPE} ${ENTITYID} -d ${TARGETDIR}
