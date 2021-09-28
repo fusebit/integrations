4710b994a8578b17d985fa9daa96edc518cb3557
Benn Bollay <bb.github@magitech.org>
2021-09-27 21:58:10 -0700

    NFC: Test publish_catalog.sh

--- scripts/cicd/publish_catalog.sh
+++ scripts/cicd/publish_catalog.sh
@@ -4,11 +4,6 @@
 echoerr() { printf "%s\n" "$*" >&2; }
 export FUSEBIT_DEBUG=
 
-# -- Optional Parameters --
-mkdir -p ~/.aws
-echo ${AWS_PROFILE_RAW} > ~/.aws/credentials
-AWS_PROFILE=${AWS_PROFILE:=default}
-
 # -- Script --
 set -ex
 
@@ -17,12 +12,12 @@ cd catalog
 node libc/index.js feed_integration > integrationsFeed.json
 node libc/index.js feed_connector > connectorsFeed.json
 
-aws --profile=${AWS_PROFILE} s3 cp --acl public-read --cache-control max-age=0 \
+aws s3 cp --acl public-read --cache-control max-age=0 \
   integrationsFeed.json \
   s3://${S3_BUCKET}/feed/integrationsFeed.json
 
-aws --profile=${AWS_PROFILE} s3 cp --acl public-read --cache-control max-age=0 \
+aws s3 cp --acl public-read --cache-control max-age=0 \
   connectorsFeed.json \
   s3://${S3_BUCKET}/feed/connectorsFeed.json
 
-aws cloudfront create-invalidation --profile=${AWS_PROFILE} --distribution-id ${CLOUDFRONT_ID} --paths '/*'
+aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths '/*'
