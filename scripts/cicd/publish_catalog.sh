84a58d76c819646105166fa584c6166aeafd28b9
Benn Bollay <benn@fusebit.io>
2021-09-27 21:10:37 -0700

    Benn/eng 690 auto version and publish catalog (#29)

---
+++ scripts/cicd/publish_catalog.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# -- Standard Header --
+echoerr() { printf "%s\n" "$*" >&2; }
+export FUSEBIT_DEBUG=
+
+# -- Optional Parameters --
+echo ${AWS_PROFILE_RAW} > ~/.aws/credentials
+AWS_PROFILE=${AWS_PROFILE:=default}
+
+# -- Script --
+set -e
+
+cd catalog
+
+node libc/index.js feed_integration > integrationsFeed.json
+node libc/index.js feed_connector > connectorsFeed.json
+
+aws --profile=${AWS_PROFILE} s3 cp --acl public-read --cache-control max-age=0 \
+  integrationsFeed.json \
+  s3://${S3_BUCKET}/feed/integrationsFeed.json
+
+aws --profile=${AWS_PROFILE} s3 cp --acl public-read --cache-control max-age=0 \
+  connectorsFeed.json \
+  s3://${S3_BUCKET}/feed/connectorsFeed.json
+
+aws cloudfront create-invalidation --profile=${AWS_PROFILE} --distribution-id ${CLOUDFRONT_ID} --paths '/*'
