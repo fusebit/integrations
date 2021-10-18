d05a43e15026180536792f4c7233bba42b993761
Matthew Zhao <matthew@fusebit.io>
2021-10-18 11:26:07 -0700

    /.

---
+++ src/linear.old/linear-provider/README.md
@@ -0,0 +1,11 @@
+# `@fusebit-int/linear-provider`
+
+> TODO: description
+
+## Usage
+
+```
+const linearProvider = require('@fusebit-int/linear-provider');
+
+// TODO: DEMONSTRATE API
+```
---
+++ src/linear/linear-connector/src/Service.ts
@@ -0,0 +1,30 @@
+import { Connector } from '@fusebit-int/framework';
+import { OAuthConnector } from '@fusebit-int/oauth-connector';
+
+class Service extends OAuthConnector.Service {
+  protected getEventsFromPayload(ctx: Connector.Types.Context) {
+    ctx.throw(500, 'Event location configuration missing. Required for webhook processing.');
+  }
+
+  protected getAuthIdFromEvent(event: any): string {
+    return '';
+  }
+
+  protected validateWebhookEvent(ctx: Connector.Types.Context): boolean {
+    ctx.throw(500, 'Webhook Validation configuration missing. Required for webhook processing.');
+  }
+
+  protected initializationChallenge(ctx: Connector.Types.Context): boolean {
+    ctx.throw(500, 'Webhook Challenge configuration missing. Required for webhook processing.');
+  }
+
+  protected async getTokenAuthId(ctx: Connector.Types.Context, token: any): Promise<string | void> {
+    return '';
+  }
+
+  protected getWebhookEventType(event: any): string {
+    return '';
+  }
+}
+
+export { Service };
