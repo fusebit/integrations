c2d2b75feb4832ddf76699267b367cbb87bc8d4d
Matthew Zhao <matthew@fusebit.io>
2021-10-26 14:58:34 -0700

    .

--- src/pagerduty/pagerduty-provider/src/PagerDutyProvider.ts
+++ src/pagerduty/pagerduty-provider/src/PagerDutyProvider.ts
@@ -1,7 +1,10 @@
 import { Internal } from '@fusebit-int/framework';
 import { api } from '@pagerduty/pdjs';
+import { PartialCall } from '@pagerduty/pdjs/build/src/api';
 
-type FusebitPagerDutyClient = typeof api & { fusebit?: any };
+// PagerDuty is a special case with it's SDK structure
+// PartialCall is the type of return when you run `await api()`
+type FusebitPagerDutyClient = PartialCall & { fusebit?: any };
 
 export default class PagerDutyProvider extends Internal.ProviderActivator<FusebitPagerDutyClient> {
   /*
@@ -9,7 +12,7 @@ export default class PagerDutyProvider extends Internal.ProviderActivator<Fusebi
    */
   protected async instantiate(ctx: Internal.Types.Context, lookupKey: string): Promise<FusebitPagerDutyClient> {
     const credentials = await this.requestConnectorToken({ ctx, lookupKey });
-    const client: FusebitPagerDutyClient = api({ token: credentials.access_token, tokenType: 'Bearer' });
+    const client: FusebitPagerDutyClient = await api({ token: credentials.access_token, tokenType: 'bearer' });
     client.fusebit = { credentials };
     return client;
   }
