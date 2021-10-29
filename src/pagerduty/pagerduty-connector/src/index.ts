c87bff6cb9d44a7526970f0df06694b5938c79d6
Matthew Zhao <matthew@fusebit.io>
2021-10-28 21:39:35 -0700

    .

--- src/pagerduty/pagerduty-connector/src/index.ts
+++ src/pagerduty/pagerduty-connector/src/index.ts
@@ -25,13 +25,14 @@ class ServiceConnector extends OAuthConnector {
 
     this.router.get('/api/configure', async (ctx: Connector.Types.Context) => {
       // Adjust the configuration elements here
-      ctx.body.uischema.elements.find((element: { label: string }) => element.label == 'OAuth2 Configuration').label =
-        'Service Configuration';
+      ctx.body.uischema.elements.find(
+        (element: { label: string }) => element.label == 'OAuth2 Configuration'
+      ).label = `${SERVICE_NAME} Configuration`;
 
       // Adjust the data schema
-      ctx.body.schema.properties.scope.description = 'Space separated scopes to request from your PagerDuty App';
-      ctx.body.schema.properties.clientId.description = 'The Client ID from your PagerDuty App';
-      ctx.body.schema.properties.clientSecret.description = 'The Client Secret from your PagerDuty App';
+      ctx.body.schema.properties.scope.description = `Space separated scopes to request from your ${SERVICE_NAME} App`;
+      ctx.body.schema.properties.clientId.description = `The Client ID from your ${SERVICE_NAME} App`;
+      ctx.body.schema.properties.clientSecret.description = `The Client Secret from your ${SERVICE_NAME} App`;
     });
   }
 }
