472a4912efb40408ec5d43537b8bdb7afa715fa7
Jacob Haller-Roby <jacob@fusebit.io>
2021-11-08 09:41:16 -0800

    webhook url alias

--- src/framework/src/client/Connector.ts
+++ src/framework/src/client/Connector.ts
@@ -208,9 +208,12 @@ class Connector<S extends Connector.Service = Connector.Service> extends EntityB
   constructor() {
     super();
 
-    this.router.post(['/api/fusebit/webhook/event', '/api/fusebit_webhook_event'], async (ctx: Connector.Types.Context) => {
-      await this.service.handleWebhookEvent(ctx);
-    });
+    this.router.post(
+      ['/api/fusebit/webhook/event', '/api/fusebit_webhook_event'],
+      async (ctx: Connector.Types.Context) => {
+        await this.service.handleWebhookEvent(ctx);
+      }
+    );
   }
 
   protected createService(): S {
