8f859449d879cbe32b157467e997786d21012731
Matthew Zhao <matthew@fusebit.io>
2021-11-17 15:43:22 -0800

    Add Linear Webhook

--- src/linear/linear-provider/src/LinearProvider.ts
+++ src/linear/linear-provider/src/LinearProvider.ts
@@ -1,55 +1,24 @@
 import { Internal } from '@fusebit-int/framework';
-import { IInstanceConnectorConfig } from '@fusebit-int/framework/libc/ConnectorManager';
 import { LinearClient as Client } from '@linear/sdk';
+import LinearWebhook from './LinearWebhook';
 
 type FusebitLinearClient = Client & { fusebit?: any };
 
 export default class LinearProvider extends Internal.ProviderActivator<FusebitLinearClient> {
+  /**
+   * This function will create an authorized webhook SDK for PagerDuty.
+   */
+  public instantiateWebhook = async (ctx: Internal.Types.Context, lookupKey: string, installId: string) => {
+    const client = await this.instantiate(ctx, lookupKey);
+    return new LinearWebhook(ctx, lookupKey, installId, this.config, client);
+  };
+
   /*
    * This function will create an authorized wrapper of the Linear SDK client.
    */
   public async instantiate(ctx: Internal.Types.Context, lookupKey: string): Promise<FusebitLinearClient> {
     const credentials = await this.requestConnectorToken({ ctx, lookupKey });
     const client: FusebitLinearClient = new Client({ accessToken: credentials.access_token });
-    client.fusebit = { credentials, webhook: new LinearWebhook(client, ctx, this.config) };
     return client;
   }
 }
-
-interface ILinearWebhookConfig {
-  teamId?: string;
-  allPublicTeams?: boolean;
-  resourceTypes: string[];
-}
-
-class LinearWebhook {
-  constructor(
-    private linearSdk: FusebitLinearClient,
-    private ctx: Internal.Types.Context,
-    private config: IInstanceConnectorConfig
-  ) {}
-
-  public async createWebhook(webhookConfig: ILinearWebhookConfig) {
-    const results = await this.linearSdk.webhookCreate({
-      url: getWebhookUrl(this.ctx, this.config),
-      ...webhookConfig,
-    });
-    return { success: results.success, webhook: results.webhook };
-  }
-
-  public async listWebhook() {
-    const results = await this.linearSdk.webhooks();
-    return results.nodes;
-  }
-
-  public async deleteWebhook(id: string): Promise<Boolean> {
-    const result = await this.linearSdk.webhookDelete(id);
-    return result.success;
-  }
-}
-
-const getWebhookUrl = (ctx: Internal.Types.Context, config: IInstanceConnectorConfig): string => {
-  const params = ctx.state.params;
-  const baseUrl = `${params.endpoint}/v2/account/${params.accountId}/subscription/${params.subscriptionId}/connector/${config.entityId}}/api/fusebit/webhook/event`;
-  return baseUrl;
-};
