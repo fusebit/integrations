a626284489be575638a70ca4cc1855ee57828d5f
Benn Bollay <bb.github@magitech.org>
2021-11-17 13:50:28 -0800

    Pass the accessToken via 'auth' not 'accessToken' to the GitHub SDK

--- src/githuboauth/githuboauth-provider/src/GitHubOAuthProvider.ts
+++ src/githuboauth/githuboauth-provider/src/GitHubOAuthProvider.ts
@@ -9,7 +9,7 @@ export default class GitHubOAuthProvider extends Internal.ProviderActivator<Fuse
    */
   public async instantiate(ctx: Internal.Types.Context, lookupKey: string): Promise<FusebitGitHubOAuthClient> {
     const credentials = await this.requestConnectorToken({ ctx, lookupKey });
-    const client: FusebitGitHubOAuthClient = new Client({ accessToken: credentials.access_token });
+    const client: FusebitGitHubOAuthClient = new Client({ auth: credentials.access_token });
     client.fusebit = { credentials };
     return client;
   }
