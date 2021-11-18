01d1c2bb2f7871bd9497e2f80b93a7da6522bbbd
degrammer <ruben@fusebit.io>
2021-11-18 18:25:50 -0500

    Fix default import for playwright.config

--- play/src/index.ts
+++ play/src/index.ts
@@ -1,4 +1,4 @@
 export * as Constants from './setup';
 export { startHttpServer, waitForExpress } from './server';
 export { IAccount, getAccount, createSession, commitSession, fusebitRequest, RequestMethod } from './sdk';
-export * as PlayWrightConfig from './playwright.config';
+export { playWrightConfig } from './playwright.config';
