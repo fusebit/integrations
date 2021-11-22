d84fd8ec95dc9bdd6feabc8e79c3a008543654d4
Benn Bollay <bb.github@magitech.org>
2021-11-22 15:45:10 -0800

    NFC: Update Atlassian play test actions

--- src/atlassian/atlassian-provider/play/actions.ts
+++ src/atlassian/atlassian-provider/play/actions.ts
@@ -8,9 +8,23 @@ export const doOAuthLogin = async (page: Page) => {
   await page.click('button:has-text("Continue")');
   await page.click('[placeholder="Enter password"]');
   await page.fill('[placeholder="Enter password"]', Constants.OAUTH_PASSWORD);
-  page.click('button:has-text("Log in")');
-  await Promise.all([page.waitForNavigation(), page.click('button:has-text("Log in")')]);
+  await page.click('button:has-text("Log in")');
 
-  // Accept the permissions page
-  await page.click('button:has-text("Accept")');
+  try {
+    await page.waitForLoadState('networkidle');
+  } catch (err) {
+    // Throws an error if the page closes, but we don't care.
+  }
+
+  if (!page.isClosed()) {
+    if (await page.$('text=Choose a site')) {
+      await page.click('text=Choose a site');
+      await page.click('#react-select-2-option-0');
+    }
+
+    if (await page.$('button:has-text("Accept")')) {
+      // Accept the permissions page
+      await page.click('button:has-text("Accept")');
+    }
+  }
 };
