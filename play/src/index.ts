6be759a0de77e80b74d2e765492e0e7405d8eb42
Benn Bollay <benn@fusebit.io>
2021-11-22 12:09:40 -0800

    Support testing proxy credentials in manage.fusebit.io (#121)

--- play/src/index.ts
+++ play/src/index.ts
@@ -2,3 +2,4 @@ export * as Constants from './setup';
 export { startHttpServer, waitForExpress } from './server';
 export { IAccount, getAccount, createSession, commitSession, fusebitRequest, RequestMethod } from './sdk';
 export { playWrightConfig } from './playwright.config';
+export * as Utilities from './utilities';
