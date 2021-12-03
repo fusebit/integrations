a16273800f6ee7b6e2407ae9259e37a43011cbc9
Shehzad Akbar <akbar.shehzad@gmail.com>
2021-12-02 19:08:50 -0500

    Jira Sample App Configs - svg, catalog, integration (#151)
    * Jira Sample App - svg, catalog, integration
    
    * Jira Sample App - catalog
    
    * git n00b
    
    * removed filtering and updated ctx.throw to flip the params around

--- catalog/feed_integration/jira/integration/integration.js
+++ catalog/feed_integration/jira/integration/integration.js
@@ -27,9 +27,11 @@ router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('
   //
   // For the Atlassian SDK documentation, see https://developer.atlassian.com/cloud/jira/platform/rest/v3/intro/.
   const atlassianClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
-  const resources = await atlassianClient.getAccessibleResources();
-
-  const jiraCloud = resources.find((resource) => resource.scopes.includes('read:jira-user'));
+  const resources = await atlassianClient.getAccessibleResources('jira');
+  if (resources.length === 0) {
+    ctx.throw(404, 'No Matching Account found in Atlassian');
+  }
+  const jiraCloud = resources[0];
   const jira = atlassianClient.jira(jiraCloud.id);
 
   const result = await jira.get('/search');
@@ -37,6 +39,27 @@ router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('
   ctx.body = {
     message: `Found ${result.total} issues in Jira Cloud ${resources[0].id}`,
   };
+});
+
+// Retrieve Issue IDs and Summaries from Jira
+// Note: This endpoint is also used by the sample app
+router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  const atlassianClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+  const resources = await atlassianClient.getAccessibleResources('jira');
+  if (resources.length === 0) {
+    ctx.throw(404, 'No Matching Account found in Atlassian');
+  }
+  const jiraCloud = resources[0];
+  const jira = atlassianClient.jira(jiraCloud.id);
+
+  const jiraIssues = await jira.get('/search?maxResults=15');
+
+  const issuesList = jiraIssues.issues.map((issues) => ({
+    issueKey: issues.key,
+    issueSummary: issues.fields.summary,
+  }));
+
+  ctx.body = issuesList;
 });
 
 module.exports = integration;
