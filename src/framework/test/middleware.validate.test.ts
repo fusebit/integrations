1acea2be7d43c47edabdcdcaa0cba2cf6eddd1db
Jacob Haller-Roby <jacob@fusebit.io>
2021-12-03 14:06:26 -0800

    Tests for authorization middleware

--- src/framework/test/middleware.validate.test.ts
+++ src/framework/test/middleware.validate.test.ts
@@ -4,6 +4,34 @@ import { request, getIntegrationConfig } from './utilities';
 
 const { Manager } = Internal;
 
+const testResource = 'testResource';
+const disallowedResource = 'invalidResource';
+
+const testActions = ['install:get', 'connector:get'];
+const disallowedActions = ['install:post', 'session:get'];
+
+const createPermissionsOptions = (
+  permissionResource = testResource,
+  actions = testActions,
+  requestedResource?: string
+) => {
+  return {
+    state: {
+      params: {
+        resourcePath: requestedResource || permissionResource,
+      },
+    },
+    caller: {
+      permissions: {
+        allow: actions.map((action) => ({
+          resource: permissionResource,
+          action,
+        })),
+      },
+    },
+  };
+};
+
 describe('Middleware: Validation', () => {
   test('Simple validation prevents invocation', async () => {
     const integration = new Integration();
@@ -40,6 +68,102 @@ describe('Middleware: Validation', () => {
 
     // Invoke the event
     await manager.handle(request('GET', '/api/test', { query: { aKey: 'foobar' } }));
+    expect(handler).toBeCalledTimes(1);
+  });
+
+  test('Authorize validation prevents access without permissions', async () => {
+    const integration = new Integration();
+
+    const handler = jest.fn();
+    integration.router.get('/api/test', integration.middleware.authorizeUser(testActions[0]), handler);
+
+    const manager = new Manager();
+    manager.setup(getIntegrationConfig(), integration.router);
+
+    // Invoke the event without permissions
+    const result = await manager.handle(request('GET', '/api/test', createPermissionsOptions(testResource, [])));
+    expect(result.status).toBe(403);
+    expect(handler).toBeCalledTimes(0);
+  });
+
+  test('Authorize validation prevents access with invalid permissions', async () => {
+    const integration = new Integration();
+
+    const handler = jest.fn();
+    integration.router.get('/api/test', integration.middleware.authorizeUser(testActions[0]), handler);
+
+    const manager = new Manager();
+    manager.setup(getIntegrationConfig(), integration.router);
+
+    // Invoke the event without permissions
+    const result = await manager.handle(
+      request('GET', '/api/test', createPermissionsOptions(testResource, disallowedActions))
+    );
+    expect(result.status).toBe(403);
+    expect(handler).toBeCalledTimes(0);
+  });
+
+  test('Authorize validation allows access', async () => {
+    const integration = new Integration();
+
+    const handler = jest.fn();
+    integration.router.get('/api/test', integration.middleware.authorizeUser(testActions[0]), handler);
+
+    const manager = new Manager();
+    manager.setup(getIntegrationConfig(), integration.router);
+
+    // Invoke the event with correct permissions
+    const result = await manager.handle(request('GET', '/api/test', createPermissionsOptions()));
+    expect(result.status).toBe(200);
+    expect(handler).toBeCalledTimes(1);
+  });
+
+  test('Wildcard authorize validation prevents access without permissions', async () => {
+    const integration = new Integration();
+
+    const handler = jest.fn();
+    integration.router.get('/api/test', integration.middleware.authorizeUser(), handler);
+
+    const manager = new Manager();
+    manager.setup(getIntegrationConfig(), integration.router);
+
+    // Invoke the event with correct permissions
+    const result = await manager.handle(request('GET', '/api/test', createPermissionsOptions(testResource, [])));
+    expect(result.status).toBe(403);
+    expect(handler).toBeCalledTimes(0);
+  });
+
+  test('Wildcard authorize validation requires matching resource', async () => {
+    const integration = new Integration();
+
+    const handler = jest.fn();
+    integration.router.get('/api/test', integration.middleware.authorizeUser(), handler);
+
+    const manager = new Manager();
+    manager.setup(getIntegrationConfig(), integration.router);
+
+    // Invoke the event with correct permissions
+    const result = await manager.handle(
+      request('GET', '/api/test', createPermissionsOptions(testResource, [], disallowedResource))
+    );
+    expect(result.status).toBe(403);
+    expect(handler).toBeCalledTimes(0);
+  });
+
+  test('Wildcard authorize validation allows access with any permissions', async () => {
+    const integration = new Integration();
+
+    const handler = jest.fn();
+    integration.router.get('/api/test', integration.middleware.authorizeUser(), handler);
+
+    const manager = new Manager();
+    manager.setup(getIntegrationConfig(), integration.router);
+
+    // Invoke the event with correct permissions
+    const result = await manager.handle(
+      request('GET', '/api/test', createPermissionsOptions(testResource, ["permission action doesn't matter"]))
+    );
+    expect(result.status).toBe(200);
     expect(handler).toBeCalledTimes(1);
   });
 });
