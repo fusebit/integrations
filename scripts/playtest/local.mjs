4dff4f6dceb254201628b844e7614bcf6952cd25
Matthew Zhao <matthew@fusebit.io>
2021-12-17 03:18:07 -0800

    .

--- scripts/playtest/local.mjs
+++ scripts/playtest/local.mjs
@@ -6,7 +6,7 @@ const fs = require('fs');
 // api.us-west-1 on.us-west-1 etc.
 // only need to match up with how storage is set.
 const DEPLOYMENT_KEY = argv._[1];
-const NO_RENAME = argv['no-rename'];
+const NORENAME = argv['norename'];
 
 const getServicesWithPlay = async () => {
   let files = await fs.promises.readdir('./src');
@@ -19,8 +19,6 @@ const getServicesWithPlay = async () => {
 };
 
 (async () => {
-  let me = (await $`git config --global --get user.name`).toString();
-  me = me.split('\n')[0];
   let storageErrors = [];
   let servicesWithPlay = await getServicesWithPlay();
   console.log(servicesWithPlay);
@@ -47,11 +45,11 @@ const getServicesWithPlay = async () => {
     console.log('WARNING WARNING WARNING: Your Branch Is Not Clean, Commit Before Testing WARNING WARNING WARNING');
     await new Promise((res) => setTimeout(res, 3000));
   }
-  if (!NO_RENAME) {
+  if (NORENAME) {
     await $`LANG=c find ./ ! -name '*.mjs' ! -name '*.sh' -type f -exec sed -i '' 's/fusebit-int/stage-fusebit/g' {} \\;`;
   }
   await $`npm i && lerna bootstrap && lerna run build`;
-  await $`./scripts/publish_all_force.sh ${PROFILE_NAME}`;
+  await $`./scripts/publish_all_force.sh`;
   await $`lerna run play:install --concurrency 1`;
   await $`./scripts/playtest/lock.mjs lock`;
   await $`lerna run play --no-bail || true`;
