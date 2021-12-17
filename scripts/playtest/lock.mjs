6dfb22323657d4a5958e18ef6332992e0445478e
Matthew Zhao <matthew@fusebit.io>
2021-12-17 02:59:47 -0800

    More tooling

--- scripts/playtest/lock.mjs
+++ scripts/playtest/lock.mjs
@@ -1,112 +1,108 @@
 #!/usr/bin/env zx
 
-const fs = require('fs')
+const fs = require('fs');
 
-const LOCK_KEY = 'lock/playwright'
-const LONG_POLL = argv['long-poll']
-const FORCE = argv['force']
+const LOCK_KEY = 'lock/playwright';
+const LONG_POLL = argv['long-poll'];
+const FORCE = argv['force'];
 
+const tryEnsureLock = async () => {
+  const lock = await tryGetLock();
+  if (lock.data.locked === 'true') {
+    console.log(`PlayWright is locked by ${lock.data.user}`);
+    process.exit(1);
+  }
+  return lock;
+};
 
-const tryEnsureLock = async() => {
-    const lock = await tryGetLock()
-    if (lock.data.locked === "true") {
-        console.log(`PlayWright is locked by ${lock.data.user}`)
-        process.exit(1)
+const pollEnsureLock = async () => {
+  let locked = true;
+  do {
+    const lock = await tryGetLock();
+    if (lock.data.locked === 'false') {
+      return lock;
     }
-    return lock
-}
+    locked = true;
+  } while (locked);
+};
 
-const pollEnsureLock = async() => {
-    let locked = true
-    do {
-        const lock = await tryGetLock()
-        if (lock.data.locked === 'false') {
-            return lock
-        }
-        locked = true
-    } while (locked)
-}
-
-const tryGetLock = async() => {
-    try {
-        return JSON.parse(await $`fuse storage get --storageId ${LOCK_KEY} -o json`)
-    } catch (_) {
-        await createNewLock()
-        return tryGetLock()
-    }
-}
+const tryGetLock = async () => {
+  try {
+    return JSON.parse(await $`fuse storage get --storageId ${LOCK_KEY} -o json`);
+  } catch (_) {
+    await createNewLock();
+    return tryGetLock();
+  }
+};
 
 // Sometimes the lock can be accidentally deleted, recreate lock here if it isn't found
-const createNewLock = async() => {
-    fs.writeFileSync('tmp', JSON.stringify({ data: { locked: 'false', user: 'Matthew Zhao' } }))
-    await $`cat tmp | fuse storage put - --storageId ${LOCK_KEY}`
-    fs.rmSync('tmp')
-}
+const createNewLock = async () => {
+  fs.writeFileSync('tmp', JSON.stringify({ data: { locked: 'false', user: 'Matthew Zhao' } }));
+  await $`cat tmp | fuse storage put - --storageId ${LOCK_KEY}`;
+  fs.rmSync('tmp');
+};
 
 const gainLock = async (lock) => {
-    lock.data.user = await whoami()
-    lock.data.locked = 'true'
-    fs.writeFileSync('tmp', JSON.stringify(lock))
-    await $`cat tmp | fuse storage put - --storageId ${LOCK_KEY}`
-    fs.rmSync('tmp')
-}
+  lock.data.user = await whoami();
+  lock.data.locked = 'true';
+  fs.writeFileSync('tmp', JSON.stringify(lock));
+  await $`cat tmp | fuse storage put - --storageId ${LOCK_KEY}`;
+  fs.rmSync('tmp');
+};
 
 const loseLock = async (lock) => {
-    lock.data.locked = 'false'
-    fs.writeFileSync('tmp', JSON.stringify(lock))
-    await $`cat tmp | fuse storage put - --storageId ${LOCK_KEY} -o json`
-    fs.rmSync('tmp')
-}
+  lock.data.locked = 'false';
+  fs.writeFileSync('tmp', JSON.stringify(lock));
+  await $`cat tmp | fuse storage put - --storageId ${LOCK_KEY} -o json`;
+  fs.rmSync('tmp');
+};
 
 const whoami = async () => {
-    return (await $`git config --global --get user.name`).toString().split('\n')[0]
-}
+  return (await $`git config --global --get user.name`).toString().split('\n')[0];
+};
 
 const writeEtag = (etag) => {
-    fs.writeFileSync('.lock', etag)
-}
+  fs.writeFileSync('.lock', etag);
+};
 
 const getEtag = () => {
-    return fs.readFileSync('.lock', 'utf-8')
-}
+  return fs.readFileSync('.lock', 'utf-8');
+};
 
-const acquireLock = async(force) => {
-    let lock;
-    if (LONG_POLL) {
-        lock = await pollEnsureLock()
-    } else if (FORCE) {
-        lock = await tryGetLock()
-    }
-     else {
-        lock = await tryEnsureLock()
-    }
-    if (force) {
-        delete lock.etag
-    }
-    await gainLock(lock)
-    lock = await tryGetLock()
-    writeEtag(lock.etag)
-}
+const acquireLock = async (force) => {
+  let lock;
+  if (LONG_POLL) {
+    lock = await pollEnsureLock();
+  } else if (FORCE) {
+    lock = await tryGetLock();
+  } else {
+    lock = await tryEnsureLock();
+  }
+  if (force) {
+    delete lock.etag;
+  }
+  await gainLock(lock);
+  lock = await tryGetLock();
+  writeEtag(lock.etag);
+};
 
 const unlock = async (force) => {
-    let lock = await tryGetLock()
-    const tag = await getEtag()
-    if (force) {
-        delete lock.etag
-    }
+  let lock = await tryGetLock();
+  const tag = await getEtag();
+  if (force) {
+    delete lock.etag;
+  }
 
-    if (lock.etag && lock.etag !== tag) {
-        console.log('An newer process have acquired the lock.')
-        process.exit(1)
-    }
+  if (lock.etag && lock.etag !== tag) {
+    console.log('An newer process have acquired the lock.');
+    process.exit(1);
+  }
 
-    await loseLock(lock)
-}
+  await loseLock(lock);
+};
 
 (async () => {
-    const action = argv._[1]
-    if (action === 'lock')
-            await acquireLock(FORCE)
-    if (action === 'unlock')
-            await unlock(FORCE)
-})()
+  const action = argv._[1];
+  if (action === 'lock') await acquireLock(FORCE);
+  if (action === 'unlock') await unlock(FORCE);
+})();
