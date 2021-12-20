49c96aa296d09c95fd14177bd5663d7752732354
Benn Bollay <bb.github@magitech.org>
2021-12-19 22:50:16 -0800

    NFC: Usability tweaks to local.mjs

--- scripts/playtest/local.mjs
+++ scripts/playtest/local.mjs
@@ -6,7 +6,12 @@ const fs = require('fs');
 // api.us-west-1 on.us-west-1 etc.
 // only need to match up with how storage is set.
 const DOMAIN_KEY = argv._[1];
-const NORENAME = argv['norename'];
+
+if (!DOMAIN_KEY) {
+  console.log('Usage:');
+  console.log(`  ${argv._[0]} on.fusebit.io [--envonly] [--norename] `);
+  process.exit(1);
+}
 
 $.verbose = false;
 
@@ -28,13 +33,16 @@ const writeEnvFiles = async (services) => {
     let storageKeys;
     try {
       storageKeys = JSON.parse(await $`fuse storage get -o json --storageId playwright/creds/${service}/${DOMAIN_KEY}`);
+      console.log(`[ ] ... ${service}`);
+      fs.unlinkSync(`src/${service}/${service}-provider/.env.playwright`);
       for (const storageKey of Object.keys(storageKeys.data)) {
-        await fs.promises.appendFile(
+        fs.appendFileSync(
           `src/${service}/${service}-provider/.env.playwright`,
           `${storageKey}=${storageKeys.data[storageKey]}\n`
         );
       }
     } catch (_) {
+      console.log(`[ ] ... \x1b[9m${service}\x1b[0m`);
       storageErrors.push(service);
       services = services.filter((svc) => svc !== service);
     }
@@ -44,30 +52,51 @@ const writeEnvFiles = async (services) => {
 };
 
 (async () => {
+  console.log(`[+] Writing credential files:`);
+  const allServices = await getServicesWithPlay();
+  await writeEnvFiles(allServices);
+
+  if (argv.envonly) {
+    process.exit(0);
+  }
+
   try {
     await $`git diff --exit-code`;
   } catch (_) {
-    console.log('WARNING WARNING WARNING: Your Branch Is Not Clean, Commit Before Testing WARNING WARNING WARNING');
+    console.log('[!] WARNING WARNING WARNING: Your Branch Is Not Clean, Commit Before Testing WARNING WARNING WARNING');
     process.exit(1);
   }
-  if (!NORENAME) {
-    const allServices = await getServicesWithPlay();
-    await writeEnvFiles(allServices);
+
+  if (!argv.norename) {
+    console.log('[+] Patching files to use scope: @stage-fusebit');
     await $`LANG=c find ./ ! -name '*.mjs' ! -name '*.sh' -type f -exec sed -i '' 's/fusebit-int/stage-fusebit/g' {} \\;`;
   }
+
+  console.log('[+] Building');
   await $`npm i && lerna bootstrap && lerna run build`;
   if (JSON.parse(fs.readFileSync('package.json')).name.includes('fusebit-int')) {
     console.log(
-      'WARNING WARNING WARNING your packages are not renamed, so you are publishing to fusebit-int. Please rename your files'
+      '[!] WARNING WARNING WARNING your packages are not renamed, so you are publishing to fusebit-int. Please rename your files'
     );
     process.exit(1);
   }
+
+  console.log('[+] Publishing');
   await $`./scripts/publish_all_force.sh`;
+
+  console.log('[+] Installing playwright');
   await $`lerna run play:install --concurrency 1`;
+
+  console.log('[>] Locking usage of the credentials for the test');
   await $`./scripts/playtest/lock.mjs lock`;
+
+  console.log('[=] Running tests');
   await $`lerna run play --no-bail || true`;
+
+  console.log('[<] Unlocking credentials');
   await $`./scripts/playtest/lock.mjs unlock`;
-  if (!NORENAME) {
+
+  if (!argv.norename) {
     await $`git stash`;
   }
 })();
