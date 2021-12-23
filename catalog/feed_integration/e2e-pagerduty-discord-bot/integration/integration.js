58eed86d34612e8dd263739ffc997a3ca2e974a5
Shehzad Akbar <akbar.shehzad@gmail.com>
2021-12-24 00:52:04 +0500

    Bencho Feedback Integrated

--- catalog/feed_integration/e2e-pagerduty-discord-bot/integration/integration.js
+++ catalog/feed_integration/e2e-pagerduty-discord-bot/integration/integration.js
@@ -18,13 +18,93 @@ router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('
 
   const incidents = await pagerdutyClient.get(`/incidents?statuses[]=triggered&since=${sinceDate}`);
 
+  if (!discordClient.fusebit.credentials.webhook) {
+    ctx.body = 'Discord Channel Webhook is not configured';
+    return;
+  }
+
   await superagent.post(discordClient.fusebit.credentials.webhook.url).send({
     content: `There have been ${incidents.resource.length} incidents triggered in the last ${days} days.`,
   });
   ctx.body = 'Message posted successfully to Discord!';
 });
 
-// Webhook that listens to & responds to incoming messages
+//Configure a new Slash Command for the Discord Bot
+//Learn more: https://discord.com/developers/docs/interactions/application-commands#slash-commands
+function slashCommandConfiguration() {
+  const command = {
+    name: 'pd',
+    description: 'PagerDuty Commands',
+    options: [
+      {
+        name: 'incident',
+        description: 'Incident related commands',
+        type: 2,
+        options: [
+          {
+            name: 'create',
+            description: 'Create new PD Incident',
+            type: 1,
+            options: [
+              {
+                name: 'title',
+                description: 'Incident Title',
+                type: 3,
+                required: true,
+              },
+              {
+                name: 'description',
+                description: 'Short Description of the Issue',
+                type: 3,
+                required: true,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  return command;
+}
+
+// Register a new slash command in a specific Guild
+router.post(
+  '/api/tenant/:tenantId/:guild/slash-command',
+  integration.middleware.authorizeUser('install:get'),
+  async (ctx) => {
+    const discordSdk = await integration.tenant.getSdkByTenant(ctx, discordConnector, ctx.params.tenantId);
+    const command = slashCommandConfiguration();
+
+    // Using the Discord Bot SDK requires an Application ID, Application Bot Token,
+    // and the 'applications.commands' scope in the Connector configuration.
+    if (!discordSdk.fusebit.credentials.applicationId) {
+      ctx.throw(404, 'Application Id not found');
+    }
+
+    //if you want to post globally instead of guild specific: `/v8/applications/${discordSdk.fusebit.credentials.applicationId}/commands`,
+    //learn more about registering commands here: https://discord.com/developers/docs/interactions/application-commands#registering-a-command
+    const response = await discordSdk.bot.post(
+      `/v8/applications/${discordSdk.fusebit.credentials.applicationId}/guilds/${ctx.params.guild}/commands`,
+      command
+    );
+    ctx.body = response;
+  }
+);
+
+// Get Guild Name & IDs for user
+router.get('/api/tenant/:tenantId/guilds', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  const discordClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+  const guilds = await discordClient.user.get(`users/@me/guilds`);
+
+  const guildDetails = guilds.map((guild) => ({
+    guildName: guild.name,
+    guildID: guild.id,
+  }));
+  ctx.body = guildDetails;
+});
+
+// Listen to and Respond to a Slash Command
 integration.event.on('/:componentName/webhook/:eventType', async (ctx) => {
   const pagerdutyClient = await integration.service.getSdk(ctx, pagerDutyConnector, ctx.req.body.installIds[0]);
 
@@ -32,14 +112,12 @@ integration.event.on('/:componentName/webhook/:eventType', async (ctx) => {
     data: { data: event, application_id, token },
   } = ctx.req.body;
 
-  // If there is no interaction object, then it's the top level message and we need to send a followup message to get more information
-  // if there is an interaction object, then we are ok to proceed and perform the final action
+  // If there is no message object, then it's the top level message and we can send a followup message to get more information
+  // if there is a message object, then we can assume it's the followup message with more details
   // Read more about interactions here: https://discord.com/developers/docs/interactions/receiving-and-responding
 
   if (!ctx.req.body.data.hasOwnProperty('message')) {
-    // The registered slash command is three levels deep
-    incidentTitle = event?.options[0]?.options[0]?.options[0]?.value;
-    incidentDescription = event?.options[0]?.options[0]?.options[1]?.value;
+    const { incidentTitle, incidentDescription } = getSlashCommandValues(event);
 
     // Get list of Services from PagerDuty
     const pdServices = await pagerdutyClient.get('/services');
@@ -73,98 +151,42 @@ integration.event.on('/:componentName/webhook/:eventType', async (ctx) => {
     });
   } else {
     const values = JSON.parse(event.values);
-    const createIncident = await pagerdutyClient.post('/incidents', {
-      data: {
-        incident: {
-          type: 'incident',
-          title: values.title,
-          service: {
-            id: values.serviceid,
-            type: 'service',
-          },
-          body: {
-            type: 'incident_body',
-            details: values.description,
-          },
-        },
-      },
-    });
-
-    // consider adding more details on the person who is on call and a link to the incident
-    await superagent.patch(`https://discord.com/api/v8/webhooks/${application_id}/${token}/messages/@original`).send({
-      content: `${createIncident.data.incident.title} has been created!`,
-    });
-  }
-});
-
-// Get Guild Name & IDs for user
-router.get('/api/tenant/:tenantId/guilds', integration.middleware.authorizeUser('install:get'), async (ctx) => {
-  const discordClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
-  const guilds = await discordClient.user.get(`users/@me/guilds`);
-
-  const guildDetails = guilds.map((guild) => ({
-    guildName: guild.name,
-    guildID: guild.id,
-  }));
-  ctx.body = guildDetails;
-});
-
-// Create a new slash command in a specific Guild
-router.post(
-  '/api/tenant/:tenantId/:guild/slash-command',
-  integration.middleware.authorizeUser('install:get'),
-  async (ctx) => {
-    const discordSdk = await integration.tenant.getSdkByTenant(ctx, discordConnector, ctx.params.tenantId);
-
-    //Create your Slash Command with the Discord Bot
-    //Learn more: https://discord.com/developers/docs/interactions/application-commands#slash-commands
-    const command = {
-      name: 'pd',
-      description: 'PagerDuty Commands',
-      options: [
-        {
-          name: 'incident',
-          description: 'Incident related commands',
-          type: 2,
-          options: [
-            {
-              name: 'create',
-              description: 'Create new PD Incident',
-              type: 1,
-              options: [
-                {
-                  name: 'title',
-                  description: 'Incident Title',
-                  type: 3,
-                  required: true,
-                },
-                {
-                  name: 'description',
-                  description: 'Short Description of the Issue',
-                  type: 3,
-                  required: true,
-                },
-              ],
+    let content = '';
+    try {
+      //Create Incident with the received details from the followup message
+      const createdIncident = await pagerdutyClient.post('/incidents', {
+        data: {
+          incident: {
+            type: 'incident',
+            title: values.title,
+            service: {
+              id: values.serviceid,
+              type: 'service',
             },
-          ],
+            body: {
+              type: 'incident_body',
+              details: values.description,
+            },
+          },
         },
-      ],
-    };
-
-    // Using the Discord Bot SDK requires an Application ID, Application Bot Token,
-    // and the 'applications.commands' scope in the Connector configuration.
-    if (!discordSdk.fusebit.credentials.applicationId) {
-      ctx.throw(404, 'Application Id not found');
+      });
+      // consider adding more details on the person who is on call and a link to the incident
+      content = `${createdIncident.data.incident.title} has been created!`;
+    } catch (e) {
+      content = `Incident creation failed, reason: ${e.message}`;
     }
 
-    //if you want to post globally instead of guild specific: `/v8/applications/${discordSdk.fusebit.credentials.applicationId}/commands`,
-    //learn more about registering commands here: https://discord.com/developers/docs/interactions/application-commands#registering-a-command
-    const response = await discordSdk.bot.post(
-      `/v8/applications/${discordSdk.fusebit.credentials.applicationId}/guilds/${ctx.params.guild}/commands`,
-      command
-    );
-    ctx.body = response;
+    await superagent
+      .patch(`https://discord.com/api/v8/webhooks/${application_id}/${token}/messages/@original`)
+      .send({ content });
   }
-);
+});
+
+// Handle the values from the slash command
+function getSlashCommandValues(event) {
+  // The registered slash command is three levels deep
+  const [title, description] = event?.options[0]?.options[0]?.options;
+  return { incidentTitle: title.value, incidentDescription: description.value };
+}
 
 module.exports = integration;
