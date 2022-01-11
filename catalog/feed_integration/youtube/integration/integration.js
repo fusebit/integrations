8e2475ba26ef110f58a3d1f248f571bf406ff681
Benn Bollay <bb.github@magitech.org>
2022-01-11 14:39:05 -0800

    Add API Docs

--- catalog/feed_integration/youtube/integration/integration.js
+++ catalog/feed_integration/youtube/integration/integration.js
@@ -22,7 +22,8 @@ const connectorName = 'googleConnector';
 // The sample test endpoint of this integration gets all contacts stored in the YouTube account associated with your tenant.
 router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
   // Create a YouTube client pre-configured with credentials necessary to communicate with your tenant's YouTube account.
-  // For the YouTube SDK documentation, see https://youtube.com/.
+  // For the YouTube SDK documentation, see https://developers.google.com/youtube/v3/docs/ and
+  // https://googleapis.dev/nodejs/googleapis/latest/youtube/.
   const google = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
 
   const youtube = google.youtube('v3');
