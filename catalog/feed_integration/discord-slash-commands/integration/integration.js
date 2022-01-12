ce584cb5f60e56d6c63da7a551ee872f212e31b3
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-01-13 02:52:35 +0500

    tested and working

--- catalog/feed_integration/discord-slash-commands/integration/integration.js
+++ catalog/feed_integration/discord-slash-commands/integration/integration.js
@@ -1,6 +1,7 @@
 // Learn more about Fusebit Integrations at: https://developer.fusebit.io/docs/integration-programming-model
 
 const { Integration } = require('@fusebit-int/framework');
+const superagent = require('superagent');
 const integration = new Integration();
 const router = integration.router;
 const connectorName = 'discordConnector';
@@ -18,20 +19,20 @@ router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('
 
 // Configure a new Slash Command for the Discord Bot
 // Learn more: https://discord.com/developers/docs/interactions/application-commands#slash-commands
-function configureSingleSlashCommand() {
+function configureSlashCommand() {
   const command = {
     name: 'command',
     description: 'Command that gets triggered',
     type: 1,
     options: [
       {
-        name: 'parameterone',
+        name: 'parameterOne',
         description: 'First parameter of the Command',
         type: 3,
         required: true,
       },
       {
-        name: 'parametertwo',
+        name: 'parameterTwo',
         description: 'Second parameter of the Command',
         type: 3,
         required: true,
@@ -41,107 +42,50 @@ function configureSingleSlashCommand() {
   return command;
 }
 
-// Configure a new Slash Command for the Discord Bot
-// Learn more: https://discord.com/developers/docs/interactions/application-commands#slash-commands
-function configureNestedSlashCommand() {
-  const command = {
-    name: 'toplevelcommand',
-    description: 'Top Level Command',
-    options: [
-      {
-        name: 'subcommand',
-        description: 'Sub Command that gets triggered',
-        type: 1,
-        options: [
-          {
-            name: 'parameterone',
-            description: 'First parameter of the Sub Command',
-            type: 3,
-            required: true,
-          },
-          {
-            name: 'parametertwo',
-            description: 'Second parameter of the Sub Command',
-            type: 3,
-            required: true,
-          },
-        ],
-      },
-    ],
-  };
-
-  return command;
-}
-
-// Configure a new Slash Command for the Discord Bot
-// Learn more: https://discord.com/developers/docs/interactions/application-commands#slash-commands
-function configureGroupedSlashCommand() {
-  const command = {
-    name: 'toplevelcommand',
-    description: 'Top Level Command',
-    options: [
-      {
-        name: 'subcommandgroup',
-        description: 'Sub Command Group',
-        type: 2,
-        options: [
-          {
-            name: 'subcommand',
-            description: 'Sub Command that gets triggered',
-            type: 1,
-            options: [
-              {
-                name: 'parameterone',
-                description: 'First parameter of the Sub Command',
-                type: 3,
-                required: true,
-              },
-              {
-                name: 'parametertwo',
-                description: 'Second parameter of the Sub Command',
-                type: 3,
-                required: true,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
-
-  return command;
-}
-
 // Register a new Slash Command in a specific Guild
 // How to Retrieve your Guild ID: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
-router.post(
-  '/api/tenant/:tenantId/:guild/slash-command',
-  integration.middleware.authorizeUser('install:get'),
-  async (ctx) => {
-    const discordSdk = await integration.tenant.getSdkByTenant(ctx, discordConnector, ctx.params.tenantId);
-    const command = slashCommandConfiguration();
-
-    // Learn more about registering commands
-    // https://discord.com/developers/docs/interactions/application-commands#registering-a-command
-    const response = await discordSdk.bot.post(
-      `/v8/applications/${discordSdk.fusebit.credentials.applicationId}/guilds/${ctx.params.guild}/commands`,
-      command
-    );
-    ctx.body = response;
-  }
-);
-
-// Register a new Slash Command globally
 router.post('/api/tenant/:tenantId/slash-command', integration.middleware.authorizeUser('install:get'), async (ctx) => {
-  const discordSdk = await integration.tenant.getSdkByTenant(ctx, discordConnector, ctx.params.tenantId);
-  const command = slashCommandConfiguration();
+  const discordClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+  const command = configureSlashCommand();
 
   // Learn more about registering commands
   // https://discord.com/developers/docs/interactions/application-commands#registering-a-command
-  const response = await discordSdk.bot.post(
-    `/v8/applications/${discordSdk.fusebit.credentials.applicationId}/commands`,
+  const response = await discordClient.bot.post(
+    `/v8/applications/${discordClient.fusebit.credentials.applicationId}/guilds/${ctx.params.guild}/commands`,
     command
   );
   ctx.body = response;
 });
+
+// Register a new Slash Command globally
+router.post('/api/tenant/:tenantId/slash-command', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  const discordClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+  const command = configureSlashCommand();
+
+  // Learn more about registering commands
+  // https://discord.com/developers/docs/interactions/application-commands#registering-a-command
+  const response = await discordClient.bot.post(
+    `/v8/applications/${discordClient.fusebit.credentials.applicationId}/commands`,
+    command
+  );
+  ctx.body = response;
+});
+
+// Listen to and Respond to a Slash Command
+integration.event.on('/:componentName/webhook/:eventType', async (ctx) => {
+  const {
+    data: { data: event, application_id, token },
+  } = ctx.req.body;
+
+  // Retrieve the parameters and handle them
+  const [parameterOne, parameterTwo] = event.options;
+  const responseMessage = `You sent me ${parameterone.value} and ${parametertwo.value} as your parameters!`;
+
+  // Send back a message to respond
+  // Read more about interactions here: https://discord.com/developers/docs/interactions/receiving-and-responding
+  await superagent
+    .post(`https://discord.com/api/v8/webhooks/${application_id}/${token}`)
+    .send({ content: responseMessage });
+});
+
 module.exports = integration;
