558a01a0eef8ab1d0c3b2df49a74e47aa7293568
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-03-11 01:42:42 +0500

    Snippets

--- catalog/feed_connector/atlassian/snippets/confluence-get-content-by-id.js
+++ catalog/feed_connector/atlassian/snippets/confluence-get-content-by-id.js
@@ -1,26 +1,35 @@
-async function atlassianConfluenceGetContentById(ctx) {
-  // For the Atlassian SDK documentation, see {enter-url-here}.
+async function atlassianConfluenceGetContentById(ctx, contentId) {
   const atlassianClient = await integration.tenant.getSdkByTenant(
     ctx,
     '<% connectorName %>',
     ctx.params.tenantId || '<% defaultTenantId %>'
   );
 
-  // TODO: replace with a really useful thing
-  // return await atlassianClient.doUsefulThing();
+  const resources = await atlassianClient.getAccessibleResources('confluence');
+  if (resources.length === 0) {
+    ctx.throw(404, 'No Matching Account found in Atlassian');
+  }
+
+  const confluenceCloud = resources[0];
+  const confluence = atlassianClient.confluence(confluenceCloud.id);
+
+  const confluencePages = await confluence.get(`/content/${contentId}`);
+
+  return confluencePages;
 }
 
 const code = `
 /**
- * {brief-jsdocs-function-description-of-a-snippet-for-intellisense}.
+ * Get Confluence Content by ID
  * 
  * @param ctx {FusebitContext} Fusebit Context
+ * @param contentId {ContentID} Id of the  content you want to retrieve
  */
 ${atlassianConfluenceGetContentById.toString()}
 `;
 
 module.exports = {
-  name: '{up-to-5-words-for-gallery-listing}',
-  description: '{up-to-3-sentences-for-gallery-details}',
+  name: 'Get Confluence Content by ID',
+  description: 'Get Confluence Content by ID',
   code,
 };
