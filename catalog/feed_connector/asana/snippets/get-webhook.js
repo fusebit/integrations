bb4dc66645a80a3b8f702167fb6a8e25c2dd39d3
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-03-12 03:09:18 +0500

    Shehzad/eng 1393 update integration templates (#256)
    * Asana Integration Template Trim
    
    * Add Snippets Reference, Push Webhooks Down as I will remove them and put them in snippets instead
    
    * Moved Webhooks to Snippets and took them out of the integration template
    
    * Matthew Hack to get around no function
    
    * Matthew Hack to get around no function V2
    
    * PR Review Updates
    
    * Pendo Success! message

---
+++ catalog/feed_connector/asana/snippets/get-webhook.js
@@ -0,0 +1,29 @@
+const code = `
+/**
+ * Get a Webhook by ID
+ * 
+ * @param ctx {FusebitContext} Fusebit Context
+ */
+
+ router.get(
+  '/api/tenant/:tenantId/webhook/:webhookId',
+  integration.middleware.authorizeUser('install:get'),
+  async (ctx) => {
+    try {
+      const asanaWebhookClient = await integration.webhook.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+
+      // API Reference: https://developers.asana.com/docs/get-a-webhook
+      const webhook = await asanaWebhookClient.get(ctx.params.webhookId);
+      ctx.body = webhook;
+    } catch (e) {
+      ctx.throw(e);
+    }
+  }
+);
+`;
+
+module.exports = {
+  name: 'Get Webhook',
+  description: 'Get a Webhook by ID',
+  code,
+};
--- catalog/feed_integration/asana/integration/integration.js
+++ catalog/feed_integration/asana/integration/integration.js
@@ -1,39 +1,27 @@
-// Fusebit Asana Integration
-//
-// This simple Asana integration allows you to call Asana APIs on behalf of the tenants of your
-// application. Fusebit manages the Asana authorization process and maps tenants of your application
-// to their Asana credentials, so that you can focus on implementing the integration logic.
-//
-// A Fusebit integration is a microservice running on the Fusebit platform.
-// You control the endpoints exposed from the microservice. You call those endpoints from your application
-// to perform specific tasks on behalf of the tenants of your app.
-//
-// Learn more about Fusebit Integrations at: https://developer.fusebit.io/docs/integration-programming-model
-
 const { Integration } = require('@fusebit-int/framework');
-
 const integration = new Integration();
 
-// Fusebit uses the KoaJS (https://koajs.com/) router to allow you to add custom HTTP endpoints
-// to the integration, which you can then call from witin your application.
+// Koa Router: https://koajs.com/
 const router = integration.router;
-
 const connectorName = 'asanaConnector';
 
-// This sample test endpoint provides the number of tasks within the tenant's first workspace
+// Test Endpoint: Return number of tasks assigned to user
 router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
   const asanaClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+
   const me = await asanaClient.users.me();
   const workspace = me.workspaces[0].gid;
   const assignee = me.gid;
+
+  // API Reference: https://developers.asana.com/docs/get-multiple-tasks
   const tasks = await asanaClient.tasks.getTasks({ workspace, assignee });
   ctx.body = {
-    message: `Found ${tasks.data.length} tasks in the Asana Workspace ${me.workspaces[0].name}`,
+    message: `Success! Found ${tasks.data.length} tasks in the Asana Workspace ${me.workspaces[0].name}`,
   };
 });
 
-// Retrieve tasks from your Asana Workspace
-// Note: This endpoint is also used by the sample app
+// Endpoint for Sample App: Retrieve tasks from your Asana Workspace
 router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('install:get'), async (ctx) => {
   const asanaClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
   const me = await asanaClient.users.me();
@@ -58,14 +46,14 @@ router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('
   ctx.body = taskDetails;
 });
 
-// Add new task to your Asana workspace
-// Note: This endpoint is also used by the sample app
+// Endpoint for Sample App: Add new task to your Asana workspace
 router.post('/api/tenant/:tenantId/item', integration.middleware.authorizeUser('install:get'), async (ctx) => {
   const asanaClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
   const me = await asanaClient.users.me();
   const workspace = me.workspaces[0].gid;
   const assignee = me.gid;
 
+  // API Reference: https://developers.asana.com/docs/create-a-task
   const tasks = await asanaClient.tasks.createTask({
     name: ctx.req.body.taskName,
     notes: ctx.req.body.taskNotes,
@@ -76,51 +64,7 @@ router.post('/api/tenant/:tenantId/item', integration.middleware.authorizeUser('
   ctx.body = tasks;
 });
 
-// The sample test endpoint registers a new webhook for use with this integration
-router.post(
-  '/api/tenant/:tenantId/webhook/-/resource/:resourceId',
-  integration.middleware.authorizeUser('install:get'),
-  async (ctx) => {
-    try {
-      const asanaWebhookClient = await integration.webhook.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
-      const webhook = await asanaWebhookClient.create(ctx.params.resourceId, {});
-      ctx.body = webhook;
-    } catch (e) {
-      ctx.throw(e);
-    }
-  }
-);
-
-// The sample test endpoint fetches a webhook by Id
-router.get(
-  '/api/tenant/:tenantId/webhook/:webhookId',
-  integration.middleware.authorizeUser('install:get'),
-  async (ctx) => {
-    try {
-      const asanaWebhookClient = await integration.webhook.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
-      const webhook = await asanaWebhookClient.get(ctx.params.webhookId);
-      ctx.body = webhook;
-    } catch (e) {
-      ctx.throw(e);
-    }
-  }
-);
-
-// The sample test endpoint deletes a webhook by Id
-router.delete(
-  '/api/tenant/:tenantId/webhook/:webhookId',
-  integration.middleware.authorizeUser('install:get'),
-  async (ctx) => {
-    try {
-      const asanaWebhookClient = await integration.webhook.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
-      const webhook = await asanaWebhookClient.delete(ctx.params.webhookId);
-      ctx.body = webhook;
-    } catch (e) {
-      ctx.throw(e);
-    }
-  }
-);
-
+// Receive Webhook Events
 integration.event.on('/asanaConnector/webhook/:eventType', (ctx) => {
   console.log('webhook received: ', ctx.req.body.data);
 });
