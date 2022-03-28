ce9fc0cfd7fe09815f95409c848292315cdee885
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-03-28 16:43:23 -0400

    salesforce

--- catalog/feed_integration/salesforce/integration/integration.js
+++ catalog/feed_integration/salesforce/integration/integration.js
@@ -1,40 +1,24 @@
-// Fusebit Salesforce Integration
-//
-// This simple Salesforce integration allows you to call Salesforce APIs on behalf of the tenants of your
-// application. Fusebit manages the Salesforce authorization process and maps tenants of your application
-// to their Salesforce credentials, so that you can focus on implementing the integration logic.
-//
-// A Fusebit integration is a microservice running on the Fusebit platform.
-// You control the endpoints exposed from the microservice. You call those endpoints from your application
-// to perform specific tasks on behalf of the tenants of your app.
-//
-// Learn more about Fusebit Integrations at: https://developer.fusebit.io/docs/integration-programming-model
-
 const { Integration } = require('@fusebit-int/framework');
-
 const integration = new Integration();
 
-// Fusebit uses the KoaJS (https://koajs.com/) router to allow you to add custom HTTP endpoints
-// to the integration, which you can then call from within your application.
+// Koa Router: https://koajs.com
 const router = integration.router;
-
 const connectorName = 'salesforceConnector';
 
-// The sample test endpoint of this integration gets all contacts stored in the Salesforce account associated with your tenant.
+// Test Endpoint: Get all contacts stored in the Salesforce account associated with your tenant
 router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
-  // Create a Salesforce client pre-configured with credentials necessary to communicate with your tenant's Salesforce account.
-  // For the Salesforce SDK documentation, see https://jsforce.github.io/.
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
   const salesforceClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
 
+  // API Reference: https://jsforce.github.io/.
   const contacts = await salesforceClient.query('SELECT count() FROM Contact');
 
   ctx.body = {
-    message: `Successfully loaded ${contacts.totalSize} contacts from SFDC`,
+    message: `Success! Loaded ${contacts.totalSize} contacts from SFDC`,
   };
 });
 
-// Retrieve Name and Email from Salesforce
-// Note: This endpoint is also used by the sample app
+// Endpoint for Sample App: Retrieve Name and Email from Salesforce
 router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('install:get'), async (ctx) => {
   const salesforceClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
   const contacts = await salesforceClient.query('SELECT name, email FROM Contact');
