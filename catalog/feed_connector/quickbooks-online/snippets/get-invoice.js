e1e9ac1a7a3f76575f12c3f2070dc2903fbdff47
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-03-28 14:57:59 -0400

    Quickbooks Snippets + Integration Template

---
+++ catalog/feed_connector/quickbooks-online/snippets/get-invoice.js
@@ -0,0 +1,26 @@
+async function quickbooksGetInvoice(ctx, invoiceID) {
+  // For the Quickbooks-online SDK documentation, see https://github.com/mcohen01/node-quickbooks
+  const quickbooksClient = await integration.tenant.getSdkByTenant(
+    ctx,
+    '<% connectorName %>',
+    ctx.params.tenantId || '<% defaultTenantId %>'
+  );
+
+  return await quickbooksClient.getInvoice(invoiceID);
+}
+
+const code = `
+/**
+ * Get an incoice from QuickBooks
+ *
+ * @param ctx {FusebitContext} Fusebit Context
+ * @param invoiceID {string} The Id of the Invoice
+ */
+${quickbooksGetInvoice.toString()}
+`;
+
+module.exports = {
+  name: 'Get an invoice from QuickBooks',
+  description: 'Get an invoice from QuickBooks',
+  code,
+};
