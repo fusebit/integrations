0f9c9aebcf08a9af166876b413c56d9a1d94a0e7
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-03-30 16:06:56 -0400

    MSFT Template + Snippets

---
+++ catalog/feed_connector/microsoft-bot-framework/snippets/remove-member.js
@@ -0,0 +1,36 @@
+async function microsoftbotframeworkRemoveMember(ctx, memberId) {
+  // For the Microsoft-bot-framework SDK documentation, see https://docs.microsoft.com/en-us/javascript/api/botbuilder/botframeworkadapter?view=botbuilder-ts-latest#botbuilder-botframeworkadapter-deleteconversationmember
+  const botFrameworkAdapter = await integration.tenant.getSdkByTenant(
+    ctx,
+    '<% connectorName %>',
+    ctx.params.tenantId || '<% defaultTenantId %>'
+  );
+
+  const { tenantId } = ctx.params;
+  const { data: conversationReference } = await integration.storage.getData(
+    ctx,
+    `/${tenantId}/teams-conversation-reference`
+  );
+
+  if (!conversationReference) {
+    ctx.throw(`Unable to find the conversation. Has ${tenantId} sent this bot a message yet?`);
+  }
+
+  return await botFrameworkAdapter.deleteConversationMember(conversationReference, memberId);
+}
+
+const code = `
+/**
+ * Remove a member from a conversation
+ * 
+ * @param ctx {FusebitContext} Fusebit Context
+ * @param memberId {string} Member ID to remove from Conversation
+ */
+${microsoftbotframeworkRemoveMember.toString()}
+`;
+
+module.exports = {
+  name: 'Remove a member from a conversation',
+  description: 'Remove a member from a conversation',
+  code,
+};
