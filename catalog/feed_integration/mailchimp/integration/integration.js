4168c6a94c31d07096f448c246e37c644d077778
degrammer <ruben@fusebit.io>
2022-05-19 13:30:30 -0500

    Change order of integration example

--- catalog/feed_integration/mailchimp/integration/integration.js
+++ catalog/feed_integration/mailchimp/integration/integration.js
@@ -5,6 +5,17 @@ const integration = new Integration();
 const router = integration.router;
 const connectorName = 'mailchimpConnector';
 
+// Test Endpoint: Ping the Mailchimp Marketing API
+router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
+  const mailchimpClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+
+  // API Reference: https://mailchimp.com/developer/marketing/guides/access-user-data-oauth-2/
+  const response = await mailchimpClient.marketing.ping.get();
+
+  ctx.body = response;
+});
+
 // Endpoint for Sample App: Retrieve audience list from your Mailchimp account
 router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('install:get'), async (ctx) => {
   // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
@@ -17,17 +28,6 @@ router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('
     audienceName: audience.name,
     activeMembers: audience.stats.member_count,
   }));
-});
-
-// Test Endpoint: Ping the Mailchimp Marketing API
-router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
-  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
-  const mailchimpClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
-
-  // API Reference: https://mailchimp.com/developer/marketing/guides/access-user-data-oauth-2/
-  const response = await mailchimpClient.marketing.ping.get();
-
-  ctx.body = response;
 });
 
 module.exports = integration;
