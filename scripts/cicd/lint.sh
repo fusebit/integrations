fcf9964d3822a848cacd13321504f002cc93df20
degrammer <ruben@fusebit.io>
2022-06-02 12:40:41 -0500

    fix ci

--- scripts/cicd/lint.sh
+++ scripts/cicd/lint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -xe
-
 npm ci
-npx lerna bootstrap
+npx lerna bootstrap --ci
 git ls-tree -r HEAD --name-only | grep -E ".[tj]?sx?$|.yaml$|.json$" | grep -v assets | grep -v lerna.json | xargs -P 1 npx prettier --check
 npx lerna run lint:check
