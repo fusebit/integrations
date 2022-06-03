cd0136c89d8ed7ce80b5f8c79d0081ceeaa5cc1f
degrammer <ruben@fusebit.io>
2022-06-03 15:45:33 -0500

    Use superagent for handling Slack Incoming Webhooks

--- src/slack/slack-provider/src/SlackIncomingWebhook.ts
+++ src/slack/slack-provider/src/SlackIncomingWebhook.ts
@@ -1,9 +1,9 @@
 import { Internal } from '@fusebit-int/framework';
-import { IncomingWebhook, IncomingWebhookResult, IncomingWebhookSendArguments } from '@slack/webhook';
+import superagent from 'superagent';
 
 export class SlackIncomingWebhook extends Internal.Provider.IncomingWebhookClient {
-  public send = async (data: string | IncomingWebhookSendArguments): Promise<IncomingWebhookResult> => {
-    const webhook = new IncomingWebhook(this.ctx.req.body.data.response_url);
-    return await webhook.send(data);
+  public send = async (data: any): Promise<any> => {
+    const response = await superagent.post(this.ctx.req.body.data.response_url).send(data);
+    return response.body;
   };
 }
