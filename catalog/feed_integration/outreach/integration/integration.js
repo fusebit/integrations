2dde4bf3469f6588a92386c4f7e7f8719bd47ff8
Benn Bollay <bb.github@magitech.org>
2022-06-05 14:20:35 -0700

    .

--- catalog/feed_integration/outreach/integration/integration.js
+++ catalog/feed_integration/outreach/integration/integration.js
@@ -1,34 +1,21 @@
-// Fusebit Outreach Integration
-//
-// This simple Outreach integration allows you to call Outreach APIs on behalf of the tenants of your
-// application. Fusebit manages the Outreach authorization process and maps tenants of your application
-// to their Outreach credentials, so that you can focus on implementing the integration logic.
-//
-// A Fusebit integration is a microservice running on the Fusebit platform.
-// You control the endpoints exposed from the microservice. You call those endpoints from your application
-// to perform specific tasks on behalf of the tenants of your app.
-//
-// Learn more about Fusebit Integrations at: https://developer.fusebit.io/docs/integration-programming-model
-
 const { Integration } = require('@fusebit-int/framework');
-
 const integration = new Integration();
 
-// Fusebit uses the KoaJS (https://koajs.com/) router to allow you to add custom HTTP endpoints
-// to the integration, which you can then call from within your application.
+// Koa Router: https://koajs.com/
 const router = integration.router;
 const connectorName = 'outreachConnector';
 
-// The sample test endpoint of this integration gets all contacts stored in the Outreach account associated with your tenant.
+// Test endpoint: Get details on the current user.
 router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
-  // Create a Outreach client pre-configured with credentials necessary to communicate with your tenant's Outreach account.
-  // For the Outreach SDK documentation, see https://outreach.com/.
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
   const outreachClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
 
-  const contacts = await outreachClient.query('SELECT count() FROM Contact');
+  // API Reference: https://api.outreach.io/api/v2/docs
+  const me = await outreachClient.get('/');
 
+  // XXX Needs to be changed
   ctx.body = {
-    message: `Successfully loaded ${contacts.totalSize} contacts from SFDC`,
+    message: `Current user: ${JSON.stringify(me)}`,
   };
 });
 
--- src/outreach/outreach-provider/src/OutreachClient.ts
+++ src/outreach/outreach-provider/src/OutreachClient.ts
@@ -2,11 +2,11 @@ import { Internal } from '@fusebit-int/framework';
 
 class OutreachClient extends Internal.Provider.ApiClient {
   public fusebit: Internal.Types.IFusebitCredentials;
-  private baseUrl = 'https://api.linkedin.com/v2';
+  private baseUrl = 'https://api.outreach.io/api/v2';
   private ctx: Internal.Types.Context;
 
   constructor(ctx: Internal.Types.Context, fusebit: Internal.Types.IFusebitCredentials) {
-    super((url: string) => `${this.baseUrl}/${url}`, fusebit.connectorId, fusebit.credentials.access_token);
+    super((url: string) => `${this.baseUrl}${url}`, fusebit.connectorId, fusebit.credentials.access_token);
     this.ctx = ctx;
     this.fusebit = fusebit;
     this.connectorId = fusebit.connectorId;
