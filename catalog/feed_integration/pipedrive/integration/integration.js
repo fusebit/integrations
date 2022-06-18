4cfc183182c4437307b333af1e9cfa0e8cd1c810
Matthew Zhao <matthewzhaocc@gmail.com>
2022-06-18 09:57:03 -0700

    .

--- catalog/feed_integration/pipedrive/integration/integration.js
+++ catalog/feed_integration/pipedrive/integration/integration.js
@@ -5,7 +5,7 @@ const integration = new Integration();
 const router = integration.router;
 const connectorName = 'pipedriveConnector';
 
-// Test Endpoint: Get all contacts stored in the Pipedrive account associated with your tenant.
+// Test Endpoint: Get all leads stored in the Pipedrive account associated with your tenant.
 router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
   // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
   const pipedriveClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
@@ -15,6 +15,27 @@ router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('
   ctx.body = {
     message: `Successfully loaded ${deals.data.length} deals from Pipedrive.`,
   };
+});
+
+// Endpoint for Sample App: Retrieve a list of leads from Pipedrive
+router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
+  const pipedriveClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+
+  const leads = await pipedriveClient.get('/v1/leads');
+
+  ctx.body = leads.data.map((lead) => lead.title);
+});
+
+// Endpoint for Sample App: Create a new lead in Pipedrive
+router.post('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
+  const pipedriveClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+
+  // Create a new lead
+  await pipedriveClient.post('/v1/leads', {
+    title: ctx.req.body.title,
+  });
 });
 
 module.exports = integration;
