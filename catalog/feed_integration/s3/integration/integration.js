4dd4bdd05385b43d0db6587fcd44c05734bcd41e
Matthew Zhao <46915203+matthewzhaocc@users.noreply.github.com>
2022-09-01 13:16:21 -0700

    AWS Catalog Entry S3 (#353)

---
+++ catalog/feed_integration/s3/integration/integration.js
@@ -0,0 +1,19 @@
+const { Integration } = require('@fusebit-int/framework');
+const AWS = require('aws-sdk');
+const integration = new Integration();
+
+// Koa Router: https://koajs.com
+const router = integration.router;
+const connectorName = 'awsConnector';
+
+// Test Endpoint: Get the buckets within the AWS account
+router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('install:get'), async (ctx) => {
+  // API Reference: https://developer.fusebit.io/reference/fusebit-int-framework-integration
+  const awsClient = await integration.tenant.getSdkByTenant(ctx, connectorName, ctx.params.tenantId);
+  const s3Sdk = awsClient.get(AWS.S3, 'us-east-1');
+  const buckets = await s3Sdk.listBuckets().promise();
+
+  ctx.body = { message: `${buckets.Buckets.length} buckets discovered from the AWS account.` };
+});
+
+module.exports = integration;
