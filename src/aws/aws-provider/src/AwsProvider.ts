69e5878ba71d85687a9fcddb90ff1e187c4bfae9
Matthew Zhao <matthewzhaocc@gmail.com>
2022-09-07 13:58:35 -0700

    .

--- src/aws/aws-provider/src/AwsProvider.ts
+++ src/aws/aws-provider/src/AwsProvider.ts
@@ -1,10 +1,9 @@
-import AWS from 'aws-sdk';
 import { Internal } from '@fusebit-int/framework';
 import AwsCredentials from './types';
 
 export default class AwsProvider extends Internal.Provider.Activator<AwsCredentials> {
   public async instantiate(ctx: Internal.Types.Context, lookupKey: string): Promise<AwsCredentials> {
-    const creds = await this.requestConnectorToken({ ctx, lookupKey });
+    const creds = (await this.requestConnectorToken({ ctx, lookupKey })) as unknown;
     return creds as AwsCredentials;
   }
 }
