17a6412ced5d296dcba1a32238f6e2a214b13b4e
Matthew Zhao <matthewzhaocc@gmail.com>
2022-10-11 16:14:59 -0700

    fixes stuff

--- catalog/feed_integration/s3/integration/integration.js
+++ catalog/feed_integration/s3/integration/integration.js
@@ -25,6 +25,10 @@ router.get('/api/tenant/:tenantId/items', integration.middleware.authorizeUser('
 
   const response = await client.listBuckets({});
 
+  if (!response.Buckets) {
+    return [];
+  }
+
   const bucketList = await Promise.all(
     response.Buckets?.map(async (bucket) => {
       const bucketDetail = await client.getBucketLocation({ Bucket: bucket.Name });
