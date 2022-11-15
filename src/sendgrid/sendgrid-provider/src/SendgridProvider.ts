e25ecaa88c1d43aef3bed86f6ddcd62fbab6f7b1
Matthew Zhao <matthewzhaocc@gmail.com>
2022-11-15 15:09:01 -0800

    .

--- src/sendgrid/sendgrid-provider/src/SendgridProvider.ts
+++ src/sendgrid/sendgrid-provider/src/SendgridProvider.ts
@@ -1,7 +1,7 @@
 import { Internal } from '@fusebit-int/framework';
-import client from '@sendgrid/client';
+import * as sendgridClient from '@sendgrid/client';
 
-type FusebitSendgridClient = typeof client & { fusebit?: Internal.Types.IFusebitCredentials };
+type FusebitSendgridClient = typeof sendgridClient & { fusebit?: Internal.Types.IFusebitCredentials };
 
 export default class SendgridProvider extends Internal.Provider.Activator<FusebitSendgridClient> {
   /*
@@ -9,14 +9,14 @@ export default class SendgridProvider extends Internal.Provider.Activator<Fusebi
    */
   public async instantiate(ctx: Internal.Types.Context, lookupKey: string): Promise<FusebitSendgridClient> {
     const credentials = await this.requestConnectorToken({ ctx, lookupKey });
-    const client_: FusebitSendgridClient = client;
-    client_.fusebit = {
+    const client: FusebitSendgridClient = sendgridClient;
+    client.fusebit = {
       credentials,
       lookupKey,
       connectorId: this.config.entityId,
     };
 
-    client_.setApiKey(credentials.access_token);
-    return client_;
+    client.setApiKey(credentials.access_token);
+    return client;
   }
 }
